421eea668b79ca632116936873c6b00779b21177
Zach | xDest <destroyer7712@gmail.com>
2018-04-07 01:52:00 -0400

    base for world

--- BitcampProject/src/com/scorpions/bcp/world/World.java
+++ BitcampProject/src/com/scorpions/bcp/world/World.java
@@ -9,8 +9,47 @@ public class World {
 	}
 	
 	
-	public void resize(int newWidth, int newHeight) {
-		//TODO resize
+	public Tile getTile(int x, int y) {
+		return worldTiles[x][y];
 	}
+	
+	public int getWorldWidth() {
+		return this.worldTiles.length;
+	}
+	
+	public int getWorldHeight() {
+		return this.worldTiles[0].length;
+	}
+	
+	
+	/**
+	 * Expand the world in x and y directions
+	 * @param expandWidth
+	 * @param expandHeight
+	 */
+	public void expand(int expandWidth, int expandHeight) {
+		Tile[][] newTiles = new Tile[getWorldWidth() + expandWidth][getWorldHeight() + expandHeight];
+		for(int i = 0; i < getWorldWidth(); i++) {
+			for(int k = 0; k < getWorldHeight(); k++) {
+				newTiles[i][k] = worldTiles[i][k];
+			}
+		}
+		if(expandWidth > 0) {
+			for(int i = 0; i < expandWidth; i++) {
+				for(int k = 0; k < getWorldHeight() + expandHeight; k++) {
+					newTiles[getWorldWidth()+i][k] = null;
+				}
+			}
+		}
+		if(expandHeight > 0) {
+			for(int i = 0; i < expandHeight; i++) {
+				for(int k = 0; k < getWorldWidth() + expandWidth; k++) {
+					newTiles[k][getWorldHeight() + i] = null;
+				}
+			}
+		}
+		this.worldTiles = newTiles;
+	}
+	
 	
 }
