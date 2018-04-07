21fc386d68f37803564b2352a828503e7e1c5666
jfreem9916 <jonahwfreeman@yahoo.com>
2018-04-07 11:04:44 -0400

    startup gui i think

--- BitcampProject/src/com/scorpions/bcp/creature/TestDriver.java
+++ BitcampProject/src/com/scorpions/bcp/creature/TestDriver.java
@@ -1,9 +1,45 @@
 package com.scorpions.bcp.creature;
 
+import java.awt.Color;
+import java.awt.Font;
+
+import javax.swing.UIManager;
+
 import com.scorpions.bcp.gui.StartupGUI;
 
 public class TestDriver {
 	public static void main(String args[]) {
+		setGUI();
 		StartupGUI start = new StartupGUI();
+	}
+	
+	public static void setGUI() {
+		Font myFont = new Font("Blackadder ITC", Font.PLAIN, 20);
+		Font myFont2 = new Font("Blackadder ITC", Font.PLAIN, 30);
+		Color myBrown = new Color(147, 88, 28);
+		Color myWhite = new Color(242, 240, 215);
+
+		UIManager.put("OptionPane.background", myBrown);
+		UIManager.put("OptionPane.messageForeground", myWhite);
+		UIManager.put("OptionPane.messageDialogTitle", myBrown);
+		UIManager.put("Panel.background", myBrown);
+		UIManager.put("OptionPane.messageFont", myFont);
+		UIManager.put("Button.background", myBrown);
+		UIManager.put("Button.foreground", myWhite);
+		UIManager.put("Button.select", myWhite);
+		UIManager.put("Button.font", myFont);
+		UIManager.put("Menu.selectionBackground", myBrown);
+		UIManager.put("Menu.selectionForeground", myBrown);
+		UIManager.put("MenuItem.selectionBackground", myWhite);
+		UIManager.put("MenuItem.selectionForeground", myBrown);
+		UIManager.put("MenuItem.font", myFont);
+		UIManager.put("Menu.font", myFont);
+		UIManager.put("TextField.background", myWhite);
+		UIManager.put("TextField.foreground", Color.BLACK);
+		UIManager.put("TextField.font", myFont);
+		
+		UIManager.put("Label.background", myBrown);
+		UIManager.put("Label.foreground", myWhite);
+		UIManager.put("Label.font", myFont2);
 	}
 }
--- BitcampProject/src/com/scorpions/bcp/gui/StartupGUI.java
+++ BitcampProject/src/com/scorpions/bcp/gui/StartupGUI.java
@@ -3,6 +3,7 @@ package com.scorpions.bcp.gui;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
@@ -11,6 +12,7 @@ public class StartupGUI extends JFrame {
 	private JPanel panel;
 	private JTextField inputBox;
 	private JButton playerB, DMB;
+	private JLabel prompt;
 	public StartupGUI() {
 		super("Bitcamp 2018 Project Startup");
 		
@@ -19,18 +21,27 @@ public class StartupGUI extends JFrame {
 		playerB = new JButton("Player");
 		DMB = new JButton("DM");
 		
+		prompt = new JLabel("Are you a player or a DM?");
+		
 		panel = new JPanel();
 		panel.add(inputBox);
 		panel.add(playerB);
 		panel.add(DMB);
+		panel.add(prompt);
 		panel.setLayout(null);
 		
-		playerB.setLocation(100, 100);
-		DMB.setLocation(100, 100);
+		playerB.setLocation(90, 145);
+		playerB.setSize(100, 40);
+		DMB.setLocation(210, 145);
+		DMB.setSize(100, 40);
+		
+		prompt.setLocation(50, 25);
+		prompt.setSize(400, 100);
 		
 		this.add(panel);
 		this.setSize(400, 225);
 		this.setResizable(false);
 		this.setVisible(true);
+		this.setLocation((1920 - this.getWidth())/2, (1080 - this.getHeight())/2);
 	}	
 }
