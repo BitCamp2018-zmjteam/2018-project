43b314cfc72f30eebd079635973b16024319f1a0
Zach | xDest <destroyer7712@gmail.com>
2018-04-07 16:14:35 -0400

    small changes

--- BitcampProject/src/com/scorpions/bcp/Driver.java
+++ BitcampProject/src/com/scorpions/bcp/Driver.java
@@ -3,12 +3,7 @@ package com.scorpions.bcp;
 public class Driver {
 
 	public static void main(String[] args) {
-		Game g = new Game();
-		g.queueEvent(new TestEvent());
-		g.eventCycle();
-		g.queueEvent(new TestEvent());
-		g.registerListener(new TestListener());
-		g.eventCycle();
+
 	}
 
 }
--- BitcampProject/src/com/scorpions/bcp/event/TradeOpenEvent.java
+++ BitcampProject/src/com/scorpions/bcp/event/TradeOpenEvent.java
@@ -4,6 +4,11 @@ import com.scorpions.bcp.Game;
 
 public class TradeOpenEvent extends Event {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 5512204445984506135L;
+
 	@Override
 	protected void enact(Game g) {
 		// TODO Auto-generated method stub
