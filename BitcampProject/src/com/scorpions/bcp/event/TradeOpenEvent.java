4024f0dd29ee1e3bbc50438097f69c54f4504bb2
Morgan <quixoticbibliomania@gmail.com>
2018-04-07 11:11:03 -0400

    Stub update for TradeOpenEvent

--- BitcampProject/src/com/scorpions/bcp/event/TradeOpenEvent.java
+++ BitcampProject/src/com/scorpions/bcp/event/TradeOpenEvent.java
@@ -1,5 +1,13 @@
 package com.scorpions.bcp.event;
 
+import com.scorpions.bcp.Game;
+
 public class TradeOpenEvent extends Event {
+
+	@Override
+	protected void enact(Game g) {
+		// TODO Auto-generated method stub
+		
+	}
 	//Stub
 }
