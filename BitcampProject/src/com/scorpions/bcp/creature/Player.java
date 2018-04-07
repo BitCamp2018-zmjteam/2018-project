6f1ae523cb2199235665eb6771ae83761cc16c17
Morgan <quixoticbibliomania@gmail.com>
2018-04-07 11:06:13 -0400

    Added auxillary requirements for dialog trees and testing

--- BitcampProject/src/com/scorpions/bcp/creature/Player.java
+++ BitcampProject/src/com/scorpions/bcp/creature/Player.java
@@ -1,15 +1,18 @@
 package com.scorpions.bcp.creature;
 
-public class Player extends Creature {
+import java.util.ArrayList;
 
+public class Player extends Creature {
+	private ArrayList<String> flags;
 	public Player(int strength, int dexterity, int constitution, int intelligence, int wisdom, int charisma) {
 		super(strength, dexterity, constitution, intelligence, wisdom, charisma);
+		flags = new ArrayList<>();
 	}
   //Stub methods - add flags later
 	public boolean hasFlag(String flag) {
-		return true;
+		return flag.contains(flag);
 	}
 	public boolean addFlag(String flag) {
-		return true;
+		return flags.add(flag);
 	}
 }
---
+++ BitcampProject/src/com/scorpions/bcp/event/TradeOpenEvent.java
@@ -0,0 +1,5 @@
+package com.scorpions.bcp.event;
+
+public class TradeOpenEvent extends Event {
+	//Stub
+}
