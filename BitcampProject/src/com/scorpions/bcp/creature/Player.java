732fff4d96b88ff7986454c7736c389a390d5990
jfreem9916 <jonahwfreeman@yahoo.com>
2018-04-07 01:12:27 -0400

    BINCH

---
+++ BitcampProject/src/com/scorpions/bcp/creature/Player.java
@@ -0,0 +1,9 @@
+package com.scorpions.bcp.creature;
+
+public class Player extends Creature {
+
+	public Player(int strength, int dexterity, int constitution, int intelligence, int wisdom, int charisma) {
+		super(strength, dexterity, constitution, intelligence, wisdom, charisma);
+	}
+
+}
