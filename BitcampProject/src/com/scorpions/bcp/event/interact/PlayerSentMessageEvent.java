e95a4cd820fee404a9f1342c3254b909e0605a30
Morgan <quixoticbibliomania@gmail.com>
2018-04-08 00:52:11 -0400

    can send text to npcs

--- BitcampProject/src/com/scorpions/bcp/event/interact/PlayerSentMessageEvent.java
+++ BitcampProject/src/com/scorpions/bcp/event/interact/PlayerSentMessageEvent.java
@@ -3,6 +3,8 @@ package com.scorpions.bcp.event.interact;
 import java.util.UUID;
 
 import com.scorpions.bcp.Game;
+import com.scorpions.bcp.creature.Creature;
+import com.scorpions.bcp.creature.NPC;
 import com.scorpions.bcp.creature.Player;
 import com.scorpions.bcp.event.Event;
 
@@ -22,7 +24,8 @@ public class PlayerSentMessageEvent extends Event {
 	@Override
 	protected void enact(Game g) {
 		// TODO Auto-generated method stub
-
+		NPC npcTarget = (NPC)Creature.getCreature(target);
+		npcTarget.sendMessage(message);
 	}
 
 }
